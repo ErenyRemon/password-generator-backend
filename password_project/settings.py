aee72128bd2fb70b06305fe665a0f8d444b3184a
Ereny Remon <161506319+ErenyRemon@users.noreply.github.com>
2025-08-12 03:08:15 +0300

    Update settings.py

--- password_project/settings.py
+++ password_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-6-4#8k+5e!2@!$(4vu3#gk(avuer3*(vmj1)58itg16r42mph1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
